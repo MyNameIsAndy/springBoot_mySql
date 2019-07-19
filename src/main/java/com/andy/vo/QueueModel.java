4527657d82bceb35cf588817a87592e77c6894f2
Andy <yunchao.yue@bj.shinsoft.net>
2019-07-19 14:54:58 +0800

    mq

---
+++ src/main/java/com/andy/vo/QueueModel.java
@@ -0,0 +1,12 @@
+package com.andy.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class QueueModel {
+    private String queueName;
+    private String dataId;
+    private String remark;
+}
